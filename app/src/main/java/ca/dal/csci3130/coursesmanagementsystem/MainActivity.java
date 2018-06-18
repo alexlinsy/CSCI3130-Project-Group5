7e013c061c5b61102272377c75e5a3cf771b670c
Alexlinsy <alexlinsy@live.com>
2018-06-18 14:22:48 -0300

    Git push testing

--- app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/MainActivity.java
+++ app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/MainActivity.java
@@ -1,13 +1,103 @@
 package ca.dal.csci3130.coursesmanagementsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("faculty").child("computerScience").child("year2").child("CSCI2110");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //Retrieve data from firebaase to textView and imageView
+                courseInfo courseInfomation = dataSnapshot.getValue(courseInfo.class);
+                TextView coursesIntro = (TextView)findViewById(R.id.courseIntro);
+
+                String courseIntroduction = "Course Schedual: " + courseInfomation.getTime() + "\n\n" + courseInfomation.getCourseIntro();
+                coursesIntro.setText(courseIntroduction);
+                TextView courseName = (TextView)findViewById(R.id.courseDisplay);
+                courseName.setText(courseInfomation.getCourseName());
+
+                TextView courseProfessor = (TextView)findViewById(R.id.professorName);
+                courseProfessor.setText(courseInfomation.getProfessor());
+
+                ImageView professorAvatar = (ImageView)findViewById(R.id.professorAvatar);
+                //Get image from Url
+                Picasso.get().load(courseInfomation.getImageUrl()).resize(400,400).into(professorAvatar);
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        final Button registerButton = (Button)findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new Button.OnClickListener() {
+            public  void onClick(View v) {
+                buttonEffect(registerButton);
+                //Change to the user page.
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+            }
+        });
+
+        final Button dropButton = (Button)findViewById(R.id.dropButton);
+        dropButton.setOnClickListener(new Button.OnClickListener() {
+            public  void onClick(View v) {
+                buttonEffect(dropButton);
+            }
+        });
+
+
+    }
+
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        if(v.getId() == R.id.registerButton) {
+                            v.getBackground().setColorFilter(Color.rgb(0, 31, 77), PorterDuff.Mode.SRC_ATOP);
+                        } else {
+                            v.getBackground().setColorFilter(Color.rgb(204, 0, 0), PorterDuff.Mode.SRC_ATOP);
+                        }
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/courseInfo.java
@@ -0,0 +1,35 @@
+package ca.dal.csci3130.coursesmanagementsystem;
+
+public class courseInfo {
+
+    private String courseIntro;
+    private String courseName;
+    private String location;
+    private String professor;
+    private String time;
+    private String imageUrl;
+
+    public courseInfo() {
+
+    }
+    public String getCourseIntro() {
+        return courseIntro;
+    }
+    public String getCourseName() {
+        return courseName;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getProfessor() {
+        return professor;
+    }
+    public String getTime() {
+        return time;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+
+}

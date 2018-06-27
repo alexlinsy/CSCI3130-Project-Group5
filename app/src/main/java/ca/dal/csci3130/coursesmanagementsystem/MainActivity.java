a2a79183abe1054fa646e6611fd30d32f8425cf6
ewafdfs <chenyangfan0726@hotmail.com>
2018-06-27 15:49:40 -0300

    [UI](Yangfan Chen & DeFang Cui) dropdown menu added

--- app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/MainActivity.java
+++ app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/MainActivity.java
@@ -2,6 +2,8 @@ package ca.dal.csci3130.coursesmanagementsystem;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get the spinner from the xml.
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        //String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.spinner1));
+//set the spinners adapter to the previously created one.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(adapter);
 
+        Spinner dropdown2 = (Spinner)findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.spinner2));
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown2.setAdapter(adapter2);
+
+        Spinner dropdown3 = (Spinner)findViewById(R.id.spinner3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.spinner3));
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown3.setAdapter(adapter3);
 
 
     }

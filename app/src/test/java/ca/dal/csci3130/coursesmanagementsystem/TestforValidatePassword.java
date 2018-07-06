c0601b15454f422ccf3db5ed333fe5c5942a2c65
RongfengCui <rongfengcui123@gmail.com>
2018-07-06 15:59:21 -0300

    [TEST] modified junit tet and already Passed all the test about password validating. Rongfeng Cui, Sha0yi Lin

--- app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestforValidatePassword.java
+++ app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestforValidatePassword.java
@@ -28,27 +28,27 @@ public class TestforValidatePassword {
     }
     @Test
     public void test5(){
-        assertTrue(valid.validate("Qfef233354535"));
+        assertTrue(!valid.validate("Qfef233354535"));
     }
     @Test
     public void test6(){
-        assertTrue(valid.validate("ASdc1234993933333"));
+        assertTrue(!valid.validate("ASdc1234993933333"));
     }
     @Test
     public void test7(){
-        assertTrue(valid.validate("Kiw92929292929292"));
+        assertTrue(!valid.validate("Kiw92929292929292"));
     }
     @Test
     public void test8(){
-        assertTrue(valid.validate("Odmei1929283213213"));
+        assertTrue(!valid.validate("Odmei1929283213213"));
     }
     @Test
     public void test9(){
-        assertTrue(valid.validate("Iwewqem22223121111"));
+        assertTrue(!valid.validate("Iwewqem22223121111"));
     }
     @Test
     public void test10(){
-        assertTrue(valid.validate("Oidk19191919191919222"));
+        assertTrue(!valid.validate("Oidk19191919191919222"));
     }
 }
 

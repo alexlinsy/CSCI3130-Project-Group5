cd1e5fce29acf3c2c757faaa3272d33d7cacd781
ewafdfs <chenyangfan0726@hotmail.com>
2018-07-09 19:27:16 -0300

    [TEST]this is the test for object programming on TA class. (huicong wu & yangfan chen)

---
+++ app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestForTaObject.java
@@ -0,0 +1,28 @@
+package ca.dal.csci3130.coursesmanagementsystem;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestForTaObject {
+    ArrayList<TA>TAs;
+    public TestForTaObject(){
+        TAs = new ArrayList<>();
+        TA ta1 =new TA("lmx","lmx@dal.ca");
+        TAs.add(ta1);
+        TA ta2 = new TA("abc","abc@dal.ca");
+    }
+    @Test
+    public void compareTaName(){
+        assertEquals(TAs.get(0).getName(),"lmx");
+        assertEquals(TAs.get(1).getName(),"adc");
+    }
+    @Test
+    public void compareTaEmail(){
+        assertEquals(TAs.get(0).getEmail(),"lmx@dal.ca");
+        assertEquals(TAs.get(1).getEmail(),"adc@dal.ca");
+    }
+
+}

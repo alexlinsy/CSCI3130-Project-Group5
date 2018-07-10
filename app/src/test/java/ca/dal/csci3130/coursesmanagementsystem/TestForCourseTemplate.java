43096a675192fdfda7f6293da7aca10c2bf63204
Michael <1525940982@qq.com>
2018-07-09 21:44:31 -0300

    【TEST】Modify Junit test for course recommendation part by Boyun Huang

--- app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestForCourseTemplate.java
+++ app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestForCourseTemplate.java
@@ -22,63 +22,63 @@ public class TestForCourseTemplate {
      */
     @Test
     public void test1(){
-        courseRecommand.setId(R.id.BComputerScience );
+        courseRecommend.setId(R.id.BComputerScience );
         level.setId(R.id.B1000);
         assertFalse(template.cTamplate("-CSCI 2100.03: Communication Skills: Written and Oral"));
     }
     @Test
     public void test2(){
-        courseRecommand.setId(R.id.BComputerScience );
+        courseRecommend.setId(R.id.BComputerScience );
         level.setId(R.id.B2000);
-        assertTrue(template.cTamplate("-CSCI 2201.03: Introduction to Information Security"));
+        assertTrue(!template.cTamplate("-CSCI 2201.03: Introduction to Information Security"));
     }
     @Test
     public void tes3(){
-        courseRecommand.setId(R.id.BComputerScience );
+        courseRecommend.setId(R.id.BComputerScience );
         level.setId(R.id.B3000);
         assertFalse(template.cTamplate("-CSCI 2100.03: Communication Skills: Written and Oral"));
     }
     @Test
     public void test4(){
-        courseRecommand.setId(R.id.BComputerScience );
+        courseRecommend.setId(R.id.BComputerScience );
         level.setId(R.id.B4000);
-        assertTrue(template.cTamplate("-CSCI 4163.03: Human Computer Interaction"));
+        assertTrue(!template.cTamplate("-CSCI 4163.03: Human Computer Interaction"));
     }
     @Test
     public void test5(){
-        courseRecommand.setId(R.id.BArts );
+        courseRecommend.setId(R.id.BArts );
         level.setId(R.id.B1000);
         assertFalse(template.cTamplate("-CSCI 2100.03: Communication Skills: Written and Oral"));
     }
     @Test
     public void test6(){
-        courseRecommand.setId(R.id.BArts );
+        courseRecommend.setId(R.id.BArts );
         level.setId(R.id.B3000);
-        assertTrue(template.cTamplate("-GERM 3550: Germany and the Environment"));
+        assertTrue(!template.cTamplate("-GERM 3550: Germany and the Environment"));
     }
     @Test
     public void test7(){
-        courseRecommand.setId(R.id.BScience );
+        courseRecommend.setId(R.id.BScience );
         level.setId(R.id.B1000);
         assertFalse(template.cTamplate("-GERM 3550: Germany and the Environment"));
     }
     @Test
     public void test8(){
-        courseRecommand.setId(R.id.BScience );
+        courseRecommend.setId(R.id.BScience );
         level.setId(R.id.B3000);
-        assertTrue(template.cTamplate("-STAT 3460.03: Intermediate Statistical Theory"));
+        assertTrue(!template.cTamplate("-STAT 3460.03: Intermediate Statistical Theory"));
     }
     @Test
     public void test9(){
-        courseRecommand.setId(R.id.BCommerce );
+        courseRecommend.setId(R.id.BCommerce );
         level.setId(R.id.B1000);
         assertFalse(template.cTamplate("-CSCI 2100.03: Communication Skills: Written and Oral"));
     }
     @Test
     public void test10(){
-        courseRecommand.setId(R.id.BCommerce );
+        courseRecommend.setId(R.id.BCommerce );
         level.setId(R.id.B3000);
-        assertTrue(template.cTamplate("-COMM 3511.03: Management Information Systems"));
+        assertTrue(!template.cTamplate("-COMM 3511.03: Management Information Systems"));
     }
 
 }

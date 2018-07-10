bb06e2b7022154bfdbe355105fe8c4d27eda58fb
ewafdfs <chenyangfan0726@hotmail.com>
2018-07-10 09:57:16 -0300

    [TEST]junit test fixed for ta object.(huicong wu & Yangfan Chen )

--- app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestForTaObject.java
+++ app/src/test/java/ca/dal/csci3130/coursesmanagementsystem/TestForTaObject.java
@@ -13,16 +13,17 @@ public class TestForTaObject {
         TA ta1 =new TA("lmx","lmx@dal.ca");
         TAs.add(ta1);
         TA ta2 = new TA("abc","abc@dal.ca");
+        TAs.add(ta2);
     }
     @Test
     public void compareTaName(){
         assertEquals(TAs.get(0).getName(),"lmx");
-        assertEquals(TAs.get(1).getName(),"adc");
+        assertEquals(TAs.get(1).getName(),"abc");
     }
     @Test
     public void compareTaEmail(){
         assertEquals(TAs.get(0).getEmail(),"lmx@dal.ca");
-        assertEquals(TAs.get(1).getEmail(),"adc@dal.ca");
+        assertEquals(TAs.get(1).getEmail(),"abc@dal.ca");
     }
 
 }

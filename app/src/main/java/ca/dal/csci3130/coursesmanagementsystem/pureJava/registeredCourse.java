d827091f2449b123d4cff301b487e31d3df59a0a
ewafdfs <chenyangfan0726@hotmail.com>
2018-07-23 00:12:55 -0300

    [Junit] New Tests made on registered course objects-InsertionSort method.(Huang JiaXing & Yangfan Chen)

--- app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/pureJava/registeredCourse.java
+++ app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/pureJava/registeredCourse.java
@@ -1,5 +1,7 @@
 package ca.dal.csci3130.coursesmanagementsystem.pureJava;
 
+import java.util.ArrayList;
+
 public class registeredCourse {
     private String courseID;
     private String courseMajor;
@@ -8,6 +10,7 @@ public class registeredCourse {
     private String startTime;
     private String endTime;
     private char[] avaliableTime;
+    private static  ArrayList<registeredCourse> registeredCourses = new ArrayList<>();
 
     public registeredCourse(){
     }
@@ -94,6 +97,30 @@ public class registeredCourse {
         avaliableTime = new char[len];
         for(int i =0 ;i < len;i++){
             avaliableTime [i] = stage2Parse2[0].charAt(i);
+        }
+    }
+
+    public static void addToList(registeredCourse course){
+        registeredCourses.add(course);
+    }
+
+    public static ArrayList<registeredCourse> getList(){
+        return registeredCourses;
+    }
+
+    public static  void InsertionSort(){
+        int i, j;
+        registeredCourse reg;
+        for(i = 1 ; i<getList().size();i++){
+            reg = getList().get(i);
+            int startTimeOnReg = Integer.parseInt(getList().get(i).getStartTime());
+            j = i - 1;
+
+            while(j >= 0 &&Integer.parseInt(getList().get(j).getStartTime()) >startTimeOnReg ){
+                getList().set(j+1, getList().get(j));
+                j = j -1;
+            }
+            getList().set(j+1,reg);
         }
     }
 }

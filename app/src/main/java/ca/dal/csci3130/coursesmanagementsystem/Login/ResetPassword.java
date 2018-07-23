090073733222f7f3c944cedc85ada343155f696b
B00735026 <xn415995@dal.ca>
2018-07-23 01:40:21 -0300

    Reset Password, (Xuanzhe Meng, Rongfeng Cui)
    Add comments

--- app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/Login/ResetPassword.java
+++ app/src/main/java/ca/dal/csci3130/coursesmanagementsystem/Login/ResetPassword.java
@@ -1,28 +1,28 @@
 package ca.dal.csci3130.coursesmanagementsystem.Login;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-
 import ca.dal.csci3130.coursesmanagementsystem.R;
 
-
+/**
+ *Class that help users reset their password
+ */
 public class ResetPassword extends AppCompatActivity {
-
+    /**
+     * User needs to provide his email address in order to reset password
+     */
     private EditText passwordemail;
     private Button passwordbutton;
     private FirebaseAuth firebaseAuth;
+    //Define the input variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +30,15 @@ public class ResetPassword extends AppCompatActivity {
         passwordemail = (EditText)findViewById(R.id.passwordEmail);
         passwordbutton = (Button)findViewById(R.id.passwordButton);
         firebaseAuth = FirebaseAuth.getInstance();
+        /**
+         * If the user enters the valid email address, then the database will send a link to the user's email address.
+         * User can follow the link to reset the password.
+         */
         passwordbutton.setOnClickListener(new View.OnClickListener() {
             @Override
+            /**
+             *
+             */
             public void onClick(View v) {
                 String resetEmail = passwordemail.getText().toString().trim();
 
@@ -45,7 +52,7 @@ public class ResetPassword extends AppCompatActivity {
                                 Toast.makeText(ResetPassword.this, "Password reset link has sent to your email.", Toast.LENGTH_SHORT).show();
                                 finish();
                             }else{
-                                Toast.makeText(ResetPassword.this,"Error! Please try again later.",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(ResetPassword.this,"Error! Invalid email address.",Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
